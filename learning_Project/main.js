6c00fc2d18cb3def43ac7b46003c0f41a6855acf
MAROUA <144175713+marwamajdoub@users.noreply.github.com>
2024-06-17 13:48:02 +0100

    commit
    commit

--- learning_Project/main.js
+++ learning_Project/main.js
@@ -1,10 +1,35 @@
 const app=Vue.createApp({
     data(){
         return{
-            product:'coffee',
-            image:'./cappucino.jpeg',
-            inventory:100
+            cart:0,
+            product:'T-shirt',
+            image:'./t_shirt.jpg',
+            inventory:100,
+            details:['50% cotton','30%wool','20%polyester'],
+            variants:[{
+                id:2234,color:'blue',image:'t_shirt.jpg'},
+                {id:2235,color:'black',image:'black.jpg'
+            },]
+         
+            
+
+            
+
+           
         }
+
+    },
+    methods:{
+        addTocart(){
+            this.cart+=1;
+
+        },
+        updateImage(variantImage){
+            this.image=variantImage;
+
+        }
+
+
     }
 
 })
